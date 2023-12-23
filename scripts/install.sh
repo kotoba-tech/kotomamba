95d79397b46f333fa9e736245c510dd28b755773
kazuki <kaz.tokyo.tech20@gmail.com>
2023-12-23 16:44:06 +0900

    env: update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+module load cuda/11.8/11.8.0
+module load cudnn/8.9/8.9.2
+module load nccl/2.16/2.16.2-1
+module load hpcx/2.12
+
 # install
 
 pip install -r requirements.txt
@@ -7,3 +12,6 @@ pip install -r requirements.txt
 # mambda
 pip install causal-conv1d>=1.1.0
 pip install mamba-ssm
+
+# multi-node
+pip install mpi4py
