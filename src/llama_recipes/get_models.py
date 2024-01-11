a6ab2fff91eb3d61bf21bb21eef637725bbea2c8
kazuki <kaz.tokyo.tech20@gmail.com>
2024-01-11 16:53:39 +0900

    fix: model dtype

--- src/llama_recipes/get_models.py
+++ src/llama_recipes/get_models.py
@@ -307,7 +307,7 @@ def get_model(
 
         model = MambaLMHeadModel.from_pretrained(
             train_config.model_name,
-            dtype=torch.float16,
+            dtype=torch.bfloat16 if train_config.use_bf16 else torch.float16,
             from_scratch=train_config.from_scratch,
             vocab_size=tokenizer.vocab_size,
         )
