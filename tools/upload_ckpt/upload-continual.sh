44af96c4648995a45db68c5f9406ca6a61c33075
kazuki <kaz.tokyo.tech20@gmail.com>
2024-02-03 03:18:59 +0900

    feat: upload script

---
+++ tools/upload_ckpt/upload-continual.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+start=10000
+end=30000
+increment=10000
+
+upload_base_dir=/bb/grandchallenge/gaf51389/converted_hf_checkpoints/mamba-2.8b/v-node
+
+# for ループで指定された範囲と増分を使用
+for ((i = start; i <= end; i += increment)); do
+  upload_dir=$upload_base_dir/iter_$(printf "%07d" $i)
+
+  python tools/upload_ckpt/upload.py \
+    --ckpt-path $upload_dir \
+    --repo-name kotoba-tech/kotomamba-2.8b-continual-ja180B-en20B-iter$(printf "%07d" $i)
+done
